00e6d4fe5be8e12f110a9a3683e38a8d77a3493f
BibiObiora <bibiavril1998@gmail.com>
2022-12-19 06:12:16 -0800

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,6 +4,7 @@
  * puts2 - print one char out of 2 of a string
  * @str: char array string type
  * Description: prints 1st char,then 3rd,then 5th,etc..
+ * Return: 0 is success
  */
 void puts2(char *str)
 {
