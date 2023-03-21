aa1cab7f88bae6e2a4aaa8dbb8c4eaa2a2155191
BibiObiora <bibiavril1998@gmail.com>
2023-03-21 02:15:34 -0700

    Updates

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
-#include "main.h"
+
 /**
  *  * _putchar - prints out a character to the stdout
  *   * @c: character to be printed
- *    * Return: an integer when
- *     */
+ *   * Return: an integer when
+ *   */
 int  _putchar(char c)
 {
-		write(1, &c, 1);
-
-			return (0);
+	return (write(1, &c, 1));
 }
